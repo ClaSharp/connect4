09f1415a83579e3c2899e728ab136679c4ea48ad
SHARP.CLAUDIA <2211143@fbisd.flourbluffschools.net>
2018-10-11 14:13:29 -0500

    wip

---
+++ player.py
@@ -0,0 +1,15 @@
+class Player:
+    def __init__(self):
+        self.name = self.get_name()
+        self.piece = piece
+        
+    def get_name(self):
+        print("get_name good")
+        pass
+    
+    def get_choice(self, board):
+        print("get_choice good")
+        pass
+    
+if __name__ == "__main__":
+    
