8330004f83fdd39d1595b819d7241c93c20fc7cd
SHARP.CLAUDIA <2211143@fbisd.flourbluffschools.net>
2018-10-12 14:12:04 -0500

    wip

--- game.py
+++ game.py
@@ -1,2 +1,14 @@
+from board import Board
+from player import Player
+
 class Game:
+    def __init__(self):
+        self.Board = board
+        self.Player = player
+        
+    def play_game():
+        
     
+if __name__ == "__main__":
+    # test code
+    pass
