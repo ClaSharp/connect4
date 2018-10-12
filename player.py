90a183c1a77e527a430e399c0b66e5d1ab104f68
SHARP.CLAUDIA <2211143@fbisd.flourbluffschools.net>
2018-10-12 14:11:17 -0500

    wip

--- player.py
+++ player.py
@@ -1,15 +1,20 @@
+
+
 class Player:
     def __init__(self):
         self.name = self.get_name()
         self.piece = piece
         
     def get_name(self):
-        print("get_name good")
-        pass
+        name = input("What is your name?\n> ")
+        return name
     
-    def get_choice(self, board):
-        print("get_choice good")
-        pass
+    def get_choice(self, Board):
+        row = width
+        
+def main():
+    self.get_name()
+    self.get_choice()
     
 if __name__ == "__main__":
-    
+    main()
