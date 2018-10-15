745d5e45ed65457d04bc0d763c1096442d46babc
SHARP.CLAUDIA <2211143@fbisd.flourbluffschools.net>
2018-10-15 14:11:07 -0500

    wip

--- game.py
+++ game.py
@@ -4,11 +4,20 @@ from player import Player
 class Game:
     def __init__(self):
         self.Board = board
-        self.Player = player
+        self.Player = [p1, p2]
+        self.turn = 0
         
-    def play_game():
+    def play_game(self):
+        
+
+def main():
+    Player()
+    print("Welcome to Connect 4!")
+    while True:
+        play_game()
+        if input("Play again?")
         
     
 if __name__ == "__main__":
     # test code
-    pass
+    main()
